6e36ebcc69be6791f94628204d8fcfd4b72744a8
senoliss <lameskauskas@gmail.com>
2026-03-25 13:34:10 +0200

    created custom hook useGame in GameContext component

--- src/Lesson 6/GameContext.tsx
+++ src/Lesson 6/GameContext.tsx
@@ -70,3 +70,13 @@ export function GameProvider({ children }: { children: React.ReactNode }) {
         </GameContext.Provider>
     );
 }
+
+export function useGame() {
+    const context = useContext(GameContext);
+
+    if(!context) {
+        throw new Error("useGame must be used inside a GameProvider");  
+    }
+
+    return context;
+}
